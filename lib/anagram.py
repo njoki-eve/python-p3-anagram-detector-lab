f8338a8e5046ac2ae4a5aa54ea6d0061b850d2ab
njoki-eve <njokieverlyne71@gmail.com>
2025-05-24 03:08:51 -0700

    first commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        return [anagram for anagram in possible_anagrams if self.is_anagram(anagram)]
+    
+    def is_anagram(self, candidate):
+        candidate_lower = candidate.lower()
+        word_lower = self.word.lower()
+        return candidate_lower != word_lower and sorted(candidate_lower) == sorted(word_lower)
--- lib/testing/anagram_test.py
+++ lib/testing/anagram_test.py
@@ -1,24 +1,11 @@
-from anagram import Anagram
+class Anagram:
+    def __init__(self, word):
+        self.word = word
 
-class TestAnagram:
-    '''Class Anagram in anagram.py'''
-
-    def test_instantiates_with_word(self):
-        '''instantiates with a single argument, a word.'''
-        assert(Anagram("word"))
-
-    def test_has_match_method(self):
-        '''contains a method called "match".'''
-        assert(Anagram.match)
-
-    def test_does_not_match_returns_empty_list(self):
-        '''returns an empty list if the input list contains no words that match the initialized word.'''
-        assert(Anagram("word").match(["hello", "goodbye"]) == [])
-
-    def test_match_one_returns_list_length_one(self):
-        '''returns a list with one element when one element of the input list matches the initialized word.'''
-        assert(Anagram("enlist").match(["listen", "poison", "hello"]) == ["listen"])
-
-    def test_match_two_returns_list_length_two(self):
-        '''returns a list with two elements when two elements of the input list match the initialized word.'''
-        assert(Anagram("enlist").match(["listen", "silent", "hippopotamus"]) == ["listen", "silent"])
+    def match(self, possible_anagrams):
+        return [anagram for anagram in possible_anagrams if self.is_anagram(anagram)]
+    
+    def is_anagram(self, candidate):
+        candidate_lower = candidate.lower()
+        word_lower = self.word.lower()
+        return candidate_lower != word_lower and sorted(candidate_lower) == sorted(word_lower)
